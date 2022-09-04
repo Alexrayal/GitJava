fdd281322530f28dc0453b7fdf1af288d6c1e817
Alexrayal <alexrayappan007@gmail.com>
2022-09-04 18:05:28 +0530

    Lines of code added

---
+++ src/org/samp/Sample.java
@@ -0,0 +1,13 @@
+package org.samp;
+
+public class Sample {
+
+	private void first() {
+		System.out.println("Process started..");
+	}
+
+	private void last() {
+		System.out.println("Process finished....");
+	}
+
+}
